99b491ad5c4a92bae992d1d52ff17c20bc2f2b9e
semdy <501859275@qq.com>
2025-08-09 16:05:15 +0800

    feat: add keepDefaultMsg option

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -18,6 +18,9 @@ function TestCom({ c }) {
             >
                 CustomCom 纯文本
             </CustomCom>
+            <Trans defaultMsg="Trans组件" />
+            <Trans id="axxxx" defaultMsg="Trans组件, 指定id" />
+            <Trans defaultMsg="Trans组件, 带values, {name}" values={{ name: '参数' }} />
         </div>
     )
 }
--- types/vite-plugin-i18n.d.ts
+++ types/vite-plugin-i18n.d.ts
@@ -1,4 +1,6 @@
 import { Plugin } from 'vite';
 import { I18nOptions } from './options';
 
+export function createFilterFn(option: Partial<I18nOptions>): (id: string) => boolean;
+
 export default function vitePluginI18n(option?: Partial<I18nOptions>): Plugin
