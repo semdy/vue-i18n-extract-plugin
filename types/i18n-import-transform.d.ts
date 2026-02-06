f17ce86dc1ca5e6fc3930fd125caa676fee18924
semdy <501859275@qq.com>
2026-02-06 11:52:38 +0800

    fix: Couldn't find a Program

--- types/i18n-import-transform.d.ts
+++ types/i18n-import-transform.d.ts
@@ -1,20 +1,17 @@
 import { NodePath } from "@babel/traverse";
-import { Node } from "@babel/types";
 import { I18nOptions } from "./options";
 
 export interface TransformResult {
-  ast: Node;
   needTransform: boolean;
 }
 
 export function i18nImportAstTransform(
-  ast: Node,
+  path: NodePath,
   importName: string,
   importPath: string
 ): TransformResult;
 
 export function babelI18nImportTransform(
   path: NodePath,
-  ast: Node,
   options: Partial<I18nOptions>
 ): boolean;
