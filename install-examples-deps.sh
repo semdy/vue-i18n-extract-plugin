db02db41c33e142100765fbf93b3d05b367a6d06
semdy <501859275@qq.com>
2026-03-03 13:00:32 +0800

    chore: update examples deps

---
+++ install-examples-deps.sh
@@ -0,0 +1,5 @@
+cd ./examples/example-AOT && yarn install
+cd ../example-cra && yarn install
+cd ../example-react-i18next && yarn install
+cd ../example-react-intl && yarn install
+cd ../example-vue && yarn install
