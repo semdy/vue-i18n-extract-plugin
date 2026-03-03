f36a353e9bd19935d94d628440b4a0744b29c200
semdy <501859275@qq.com>
2026-03-03 13:07:47 +0800

    chore: update install-examples-deps.sh

--- install-examples-deps.sh
+++ install-examples-deps.sh
@@ -1,5 +1,5 @@
-cd ./examples/example-AOT && yarn add extract-i18n-plugin -D && yarn install
-cd ../example-cra && yarn add extract-i18n-plugin -D && yarn install
-cd ../example-react-i18next && yarn add extract-i18n-plugin -D && yarn install
-cd ../example-react-intl && yarn add extract-i18n-plugin -D && yarn install
-cd ../example-vue && yarn add extract-i18n-plugin -D && yarn install
+cd ./examples/example-AOT && yarn add extract-i18n-plugin@latest -D && yarn install
+cd ../example-cra && yarn add extract-i18n-plugin@latest -D && yarn install
+cd ../example-react-i18next && yarn add extract-i18n-plugin@latest -D && yarn install
+cd ../example-react-intl && yarn add extract-i18n-plugin@latest -D && yarn install
+cd ../example-vue && yarn add extract-i18n-plugin@latest -D && yarn install
