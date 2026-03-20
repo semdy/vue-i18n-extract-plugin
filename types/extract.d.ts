1a770ed1ae2ac114e632a649ccec3138610a9d27
semdy <501859275@qq.com>
2026-03-20 13:26:12 +0800

    refactor

--- types/extract.d.ts
+++ types/extract.d.ts
@@ -1,24 +1,3 @@
-import { Node } from "@babel/types";
 import { I18nOptions as Options } from "./options";
-import { I18nMap } from "./common";
-
-export interface WriteResult {
-  hasDiff: boolean;
-  data: I18nMap;
-}
-
-export function writeI18nMapToFile(
-  i18nMap: I18nMap,
-  options: Partial<Options>,
-  checkDiffs?: boolean
-): Promise<WriteResult>;
-
-export function handleFinalI18nMap(
-  i18nMap: I18nMap,
-  options: Partial<Options>,
-  checkDiffs?: boolean
-): Promise<void>;
 
 export function extractI18n(options: Partial<Options>): Promise<void>;
-
-export let globalI18nMap: I18nMap;
